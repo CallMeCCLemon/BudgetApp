ddaeaa586774261c629a41502bd44775bf943475
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2024-04-28 21:25:48 -0700

    Adding integration tests for remaining CRUD operations

--- service/service_integ_test.go
+++ service/service_integ_test.go
@@ -3,6 +3,7 @@ package service
 import (
 	"BudgetingApp/persistance"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -11,7 +12,7 @@ import (
 	"testing"
 )
 
-func Test_get_budgets_api_integration_tests(t *testing.T) {
+func Test_budget_CRUD_operations_tests(t *testing.T) {
 	dao, err := persistance.NewStorageDao(os.Getenv("USERNAME"), os.Getenv("PASSWORD"), os.Getenv("HOST"), "budgetApp")
 	if err != nil {
 		return
@@ -19,29 +20,19 @@ func Test_get_budgets_api_integration_tests(t *testing.T) {
 	ts := httptest.NewServer(setupServer(dao))
 	defer ts.Close()
 
-	t.Run("it should return all budgets which exist", func(t *testing.T) {
-		resp, err := http.Get(fmt.Sprintf("%s/budget", ts.URL))
+	client := &http.Client{}
+
+	budgetName := "integ-test-budget-001"
+
+	budget := Budget{
+		Name: budgetName,
+	}
+
+	t.Run("Create a budget", func(t *testing.T) {
+		body, err := json.Marshal(budget)
 		if err != nil {
-			t.Fatalf("Expected no error, got %v", err)
+			return
 		}
-		buf := new(bytes.Buffer)
-		_, _ = buf.ReadFrom(resp.Body)
-		println(buf.String())
-		assert.Equal(t, http.StatusOK, resp.StatusCode)
-
-	})
-}
-
-func Test_post_budget_api_integration_tests(t *testing.T) {
-	dao, err := persistance.NewStorageDao(os.Getenv("USERNAME"), os.Getenv("PASSWORD"), os.Getenv("HOST"), "budgetApp")
-	if err != nil {
-		return
-	}
-	ts := httptest.NewServer(setupServer(dao))
-	defer ts.Close()
-
-	t.Run("it should create a new budget", func(t *testing.T) {
-		body := []byte(`{"name": "integ-test-budget-001"}`)
 
 		resp, err := http.Post(
 			fmt.Sprintf("%s/budget", ts.URL),
@@ -54,7 +45,48 @@ func Test_post_budget_api_integration_tests(t *testing.T) {
 		buf := new(bytes.Buffer)
 		_, _ = buf.ReadFrom(resp.Body)
 		println(buf.String())
-		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		var newBudget Budget
+		_ = json.Unmarshal(buf.Bytes(), &newBudget)
 
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, budget.Name, newBudget.Name)
+		budget = newBudget
+	})
+
+	t.Run("Read a single budget", func(t *testing.T) {
+		resp, err := http.Get(fmt.Sprintf("%s/budget/%d", ts.URL, budget.ID))
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(resp.Body)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("Delete a budget", func(t *testing.T) {
+		req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/budget/%d", ts.URL, budget.ID), nil)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		resp, err := client.Do(req)
+
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(resp.Body)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("Read all budgets", func(t *testing.T) {
+		resp, err := http.Get(
+			fmt.Sprintf("%s/budget", ts.URL),
+		)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		buf := new(bytes.Buffer)
+		_, _ = buf.ReadFrom(resp.Body)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
 	})
 }
