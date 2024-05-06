d4f5b4f4b6c0ff3d3907340116729cb1615b2e45
Caleb Littlejohn <43481221+Caleb-Littlejohn@users.noreply.github.com>
2024-05-06 13:54:34 -0400

    Add budget dropdown to html

--- web-app/components/navbar/navbar.tsx
+++ web-app/components/navbar/navbar.tsx
@@ -2,11 +2,21 @@ import Link from "next/link";
 import './navbar.css';
 
 export default function Navbar() {
+    let budgets = ["Budget 1", "Budget 2", "Budget 3"]
+
     return (
         <main className="flex navbar">
             <ul className="horizontal">
                 <li><Link href="/">Home</Link></li>
-                <li><Link href="/budget">Budget</Link></li>
+                <li className="dropdown">
+                    <Link href="/budget">Budget</Link>
+                    <div className="dropdown-content">
+                        {budgets.map(budget => {
+                            let link = '/budget/' + encodeURI(budget);
+                            return <Link key={budget} href={link}>{budget}</Link>
+                        })}
+                    </div>
+                </li>
                 <li className="dropdown">
                     <Link href="javascript:void(0)" className="dropbtn">Account</Link>
                     <div className="dropdown-content right">
